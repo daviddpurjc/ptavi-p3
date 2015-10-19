e7bd55a02e67457195801d16e763a918dcc7a357
david <david.duran.prieto@gmail.com>
2015-10-20 01:29:20 +0200

    [Actualización]

--- karaoke.py
+++ karaoke.py
@@ -9,11 +9,53 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
-if __name__ == "__main__":
+class KaraokeLocal():
     """
     Programa principal
     """
+    def __init__ (self):
+        self.original = ""
+        self.resultante = ""
+        parser = make_parser()
+        ccHandler = SmallSMILHandler()
+        parser.setContentHandler(ccHandler)
+        parser.parse(open(sys.argv[1]))
+        self.etiquetas = ccHandler.get_tags()
+
+    def __str__ (self):
+        cadena = ''
+        for linea in self.etiquetas:       
+            cadena = cadena + linea[0]
+            dicc = linea[1]
+            for elem in dicc:
+                if dicc[elem] != "":
+                    cadena = cadena + '\t' + elem + '="' + dicc[elem] + '"\t'
+
+        return (cadena)
+
+    def do_local(self):
+        ccHandler = SmallSMILHandler()
+        for linea in ccHandler.get_tags():       
+            dicc = linea[1]
+            for elem in dicc:
+                if elem == 'src':
+                    if dicc[elem].startswith('http://'):
+                        ddp2 = urllib.request.urlopen(dicc[elem])
+                        ddp2.read()
+                        ddp2.close()
+                        indice = str.rfind(dicc[elem], '/')
+                        dicc[elem] = dicc[elem][indice+1:]
+
+    def do_json(self, original, resultante):
+        if self.resultante == None:
+            json.dump(self.etiquetas, open("karaoke.json",'w'), sort_keys=True, indent=4, separators=(',', ': '))
+        else:
+            json.dump(self.etiquetas, open("locaL.json",'w'), sort_keys=True, indent=4, separators=(',', ': '))
+
+if __name__ == "__main__":
+
     try:
+        pr = KaraokeLocal()
         parser = make_parser()
         ccHandler = SmallSMILHandler()
         parser.setContentHandler(ccHandler)
@@ -21,24 +63,11 @@ if __name__ == "__main__":
     except:
         sys.exit("Usage: python3 karaoke.py file.smil.")
 
-    for linea in ccHandler.get_tags():       
-        dicc = linea[1]
-        for elem in dicc:
-            if elem == 'src':
-                if dicc[elem].startswith('http://'):
-                    ddp2 = urllib.request.urlopen(dicc[elem])
-                    ddp2.read()
-                    ddp2.close()
-                    indice = str.rfind(dicc[elem], '/')
-                    dicc[elem] = dicc[elem][indice+1:]
+    print (pr)
+    pr.do_json(pr.original,pr.resultante)
+    pr.do_local()
+    pr.do_json(pr.original,"local.json")
+    print (pr)
 
-    for linea in ccHandler.get_tags():       
-        print (linea[0], end="\t")
-        dicc = linea[1]
-        for elem in dicc:
-            if dicc[elem] != "":
-                print (elem, dicc[elem], sep='="', end='"\t')
-        print()
+    
 
-    misdatos = ccHandler.get_tags()
-    json.dump(misdatos, open("karaoke.json",'w'), sort_keys=True, indent=4, separators=(',', ': '))
